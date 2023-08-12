cddf11c33977995653ce3b4e00af80faf9f9130c
Joon Song <js30@illinois.edu>
2023-08-12 22:06:46 +0900

    jsnb

---
+++ app/(site)/page.tsx
@@ -0,0 +1,43 @@
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import AuthForm from './components/AuthForm';
+
+
+export default function Home() {
+  return (
+    <div
+      className="
+      flex
+      min-h-full
+      flex-col
+      justify-center
+      py-12
+      sm:px-6
+      lg:px-8
+      bg-gray-100"
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt='Logo'
+          height='96'
+          width='96'
+          className='mx-auto w-auto'
+          src='/image/mglogo.png'
+        />
+        <h2
+          className="
+          mt-6
+          text-center
+          text-3xl
+          font-bold
+          tracking-tight
+          text-gray-900"
+        >
+          Log In to use JSN ßeta
+        </h2>
+      </div>
+      <AuthForm/>
+    </div>
+
+  )
+}
